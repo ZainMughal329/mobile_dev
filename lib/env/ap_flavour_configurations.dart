695ad81dbf63ad3b55d1028928315a1f07652fbc
Hanan <hanan.khalid@algorepublic.com>
2024-03-08 17:26:27 +0500

    working on metsimaholo flavour

--- lib/env/ap_flavour_configurations.dart
+++ lib/env/ap_flavour_configurations.dart
@@ -2,8 +2,8 @@ class AppFlavoursConfigurations {
 
   static const configLasediPro = {
     "app_name": "Lasedi",
-  "base_url": "http://185.193.67.250:3000/",
-    "app_logo": "assets/images/logo_lesedi.png",
+  "base_url": "http://185.193.67.250/",
+    "app_logo": "assets/images/logos_and_icons/logo_lesedi.png",
     "app_icon": "...........",
     "env": "Lasedi"
   };
@@ -16,17 +16,23 @@ class AppFlavoursConfigurations {
   };
   static const configModimollePro = {
     "app_name": "Modimolle",
-    "base_url": "http://185.193.67.250:3000/",
-    "app_logo": "assets/images/moiloa.png",
+    "base_url": "http://173.249.14.72/",
+    "app_logo": "assets/images/logos_and_icons/modimolle_logo.png",
     "app_icon": "......",
     "env": "modimolle"
   };
   static const configRemotesherePro = {
     "app_name": "Remoteshere",
-    "base_url": "http://185.193.67.250:3000/",
-    "app_logo": "assets/images/ramotshere.jpg",
-    // "app_logo": "assets/images/moiloa.png",
+    "base_url": "http://173.249.14.72:3000/",
+    "app_logo": "assets/images/logos_and_icons/ramoteshere_logo.png",
     "app_icon": "......",
     "env": "remoteshere"
+  };
+  static const configMetsimaholoPro = {
+    "app_name": "Metsimaholo",
+    "base_url": "http://185.193.67.250:3001/",
+    "app_logo": "assets/images/logos_and_icons/ramoteshere_logo.png",
+    "app_icon": "......",
+    "env": "metsimaholo"
   };
 }
